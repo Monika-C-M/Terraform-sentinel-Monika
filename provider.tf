fd529a93722e0d33448a90c7a1d91084a36e5dd4
Monika-C-M <monika.c.m@gds.ey.com>
2023-04-10 17:25:56 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.62.0"
+    }
+  }
+}
+
+provider "aws" {
+   access_key = "AKIA4DEG57NMZGVKNA73"
+   secret_key = "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"
+   region = "us-west-2"
+}
