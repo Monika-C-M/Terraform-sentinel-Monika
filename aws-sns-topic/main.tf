94565ee0e3e81b52e71f7b271a0263a92240b529
Monika-C-M <monika.c.m@gds.ey.com>
2023-04-14 11:59:48 +0530

    pushing branch

---
+++ aws-sns-topic/main.tf
@@ -0,0 +1,18 @@
+  resource "aws_kms_key" "Sns_Sqs_key" {
+  description             = "KMS key sns and sqs"
+  deletion_window_in_days = 15
+}
+
+output "key_id" {
+  value= aws_kms_key.Sns_Sqs_key.*
+}
+
+resource "aws_sns_topic" "sns_topic" {
+  name              = "first-SNS-topic-1"
+  /*kms_master_key_id = "alias/aws/sns"*/
+  kms_master_key_id = "${aws_kms_key.Sns_Sqs_key.key_id}"
+}
+
+output "sns_output" {
+  value= aws_sns_topic.sns_topic.*
+}
